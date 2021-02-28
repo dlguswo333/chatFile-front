6ebbc41e64fce28a60776c347db9671dffc9e56d
dlguswo333 <dlguswo333@skku.edu>
2021-02-28 19:48:17 +0900

    add basic change password implementation

--- src/Settings.js
+++ src/Settings.js
@@ -4,32 +4,62 @@ import './Settings.css';
 class Settings extends Component {
   constructor(props) {
     super(props);
-    this.passwordRef = React.createRef();
+    this.pwRef = React.createRef();
+    this.newPwRef = React.createRef();
+    this.reNewPwRef = React.createRef();
   }
+
   checkPassword(pw) {
     // The function does not validate the password.
     // It only checks whether the password input is empty or not!
     if (!pw) {
       // Password is empty. Show warning.
-      alert("Please insert your password.");
+      alert("Please type your current password.");
       return false;
     }
     return true;
+  }
+
+  changePassword = (pw) => {
+    // This function is called after checkPassword function.
+    const newPw = this.newPwRef.current.value;
+    const reNewPw = this.reNewPwRef.current.value;
+    if (newPw !== reNewPw) {
+      alert("New passwords do not match! Please check it again.");
+      return false;
+    }
+    // TODO Call props.changePassword function here.
   }
   render() {
     return (
       <div className="SettingsWindow" ref={this.props.innerRef}>
         <h3>Settings</h3>
         <div className="Wrapper">
-          <span className="Attribute">id:</span>
+          <span className="Attribute">ID:</span>
           <span className="Value">{this.props.myId}</span>
         </div>
         <div className="Wrapper">
-          <span className="Attribute">password:</span>
-          <input type="password" className="Value SettingsInput" ref={this.passwordRef}></input>
+          <span className="Attribute">Password:</span>
+          <input type="password" className="Value SettingsInput" ref={this.pwRef}></input>
         </div>
+        <div className="Wrapper">
+          <span className="Attribute">New Password:</span>
+          <input type="password" className="Value SettingsInput" ref={this.newPwRef}></input>
+        </div>
+        <div className="Wrapper">
+          <span className="Attribute">Retype New Password:</span>
+          <input type="password" className="Value SettingsInput" ref={this.reNewPwRef}></input>
+        </div>
+        <button className="ChangePwButton" onClick={() => {
+          const pw = this.pwRef.current.value;
+          if (this.checkPassword(pw)) {
+            this.changePassword(pw);
+          }
+        }}>
+          Change Password
+        </button>
         <button className="DeleteButton" onClick={() => {
-          const pw = this.passwordRef.current.value;
+          const pw = this.pwRef.current.value;
           if (this.checkPassword(pw)) {
             this.props.deleteAccount(pw);
           }
